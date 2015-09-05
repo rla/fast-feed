c9d42240d14be037a4144d820a5187e398fe95cf
Sascha Depold <sascha@depold.com>
2015-09-05 13:49:23 +0200

    Add categories to feed items

--- tests/other_content.js
+++ tests/other_content.js
@@ -4,10 +4,23 @@ var parser = require('../');
 var rss = '<rss><channel><title>Test</title>' +
     '<item><title>T1</title></item>ABC<item><title></title></item></channel></rss>';
 
+var rssWithCategories = '<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">' +
+    '<channel><item><category>Motosport</category><category>Cars</category>'+
+    '</item></channel></rss>';
+
 describe('Parser', function() {
 
     it('should accept empty title', function() {
         var feed = parser.parse(rss);
         assert.equal(feed.items.length, 2);
+    });
+
+    it('should find the categories', function () {
+      var feed = parser.parse(rssWithCategories);
+      assert.equal(feed.items.length, 1);
+      assert.equal(
+        JSON.stringify(feed.items[0].categories),
+        JSON.stringify([ 'Motosport', 'Cars' ])
+      );
     });
 });
